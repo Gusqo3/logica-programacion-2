f735fa948d5163fcb89bb2ced6d8a7bfad563dc4
Gusqo3 <gquirozo1600@gmail.com>
2026-05-07 15:32:43 -0600

    Cambio de input a tipo text para validar puntos, validacion de campos vacios

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-function convertir(){
+function convertir() {
     const gradosCelsius = document.getElementById("grCelsius").value;
     const divConv = document.getElementById("conversion");
 
-    const gradosF = ((9/5) * gradosCelsius) + 32;
+    const gradosF = ((9 / 5) * gradosCelsius) + 32;
     const gradosK = parseFloat(gradosCelsius) + 273.15;
 
-    const pgradosC = document.createElement("p");
-    pgradosC.innerText = `Grados Celsius: ${gradosCelsius}
+    if (gradosCelsius !== "") {
+        const pgradosC = document.createElement("p");
+        pgradosC.innerText = `Grados Celsius: ${gradosCelsius}
                         Grados Fahrenheit: ${gradosF}
                         Grados Kelvin: ${gradosK}`;
-    divConv.append(pgradosC);
+        divConv.append(pgradosC);
+    } else
+        alert("Completa los campos");
+
 }
 
 const input = document.getElementById("grCelsius");
 
-// No acepta puntos :c
 input.addEventListener("input", () => {
     input.value = input.value.replace(/[^0-9.]/g, "");
 });
